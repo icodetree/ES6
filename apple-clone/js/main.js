d9a1ee2101448a000140d4d5e36d4d7040d99f1e
icodetree <icodetree@naver.com>
2023-05-05 23:53:55 +0900

    add css

---
+++ apple-clone/js/main.js
@@ -0,0 +1,10 @@
+//1. 모든애니메이션에 대한 정보를 배열에 담아둔다.
+(() => {
+    // 2. 섹션별 배열을 생성해준다.
+    const sceneInfo = [
+        {
+            heightNum : 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+            scrollHeight : 0,
+        }
+    ]
+})
